ec4e26153e19632ea7bcf2c70d3f8744de6c2de9
NamanKarki-nbk <namankarki.nbk@gmail.com>
2025-04-06 02:26:09 +0545

    crud completed

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,29 @@
+import {Request, Response} from 'express';
+import prisma from '../prisma/client';
+
+export const getUser = async(req:Request, res:Response):Promise<void>=>{
+    const user = await prisma.user.findMany();
+    res.json(user);
+};
+
+export const createUser = async(req: Request, res: Response):Promise<void>=>{
+    const {name,email}=req.body;
+    const user = await prisma.user.create({data:{name,email}});
+    res.json(user);
+};
+
+export const updateUser = async(req: Request, res:Response):Promise<void>=>{
+    const {id} = req.params;
+    const {name, email}=req.body;
+    const user = await prisma.user.update({
+        where: {id:Number(id)},
+        data:{name,email},
+    });
+    res.json(user);
+};
+
+export const deleteUser = async (req:Request, res:Response): Promise<void>=>{
+    const {id} = req.params;
+    await prisma.user.delete({where:{id: Number(id)}});
+    res.json({message: 'User deleted'});
+};
